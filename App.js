d8b18b8f0001935b33805f4b2d95a47521c18824
sungahid2004-hub <sungahid2004@gmail.com>
2025-09-25 08:22:54 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,16 @@
 import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-
+import CounterApp from './CounterApp.js';
 export default function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.number}>{count}</Text>
-      <View style={styles.buttonRow}>
-        <Button title="Increment" onPress={() => setCount(count + 1)} />
-        <Button title="Decrement" onPress={() => setCount(count - 1)} />
-      </View>
-    </View>
-  );
+return(
+  <view style ={styles.container}>
+<CounterApp/>
+  </View>
+  )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5"
-  },
-  number: {
-    fontSize: 48,
-    marginBottom: 20
-  },
-  buttonRow: {
-    flexDirection: "row",
-    gap: 10
+  container:{
+    flex:1
   }
 });
